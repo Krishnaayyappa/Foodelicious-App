0b72b62d9b3561e8093773e803f9c7cccbf3a851
Hari krishna Ayyappa Narina <krishnanarina8@gmail.com>
2022-11-14 17:30:42 -0500

    form validation update

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -4,25 +4,41 @@ import { useContext, useState } from "react";
 import CartContext from "../../store/Cart-context";
 import CartItem from "./CartItem";
 import Checkout from "./checkout";
+import React from "react";
 
 
 
 const Cart = (props) => {
     const cartCtx = useContext(CartContext);
     const [checkout, setCheckout] = useState(false);
+    const [isloading, setLoading] = useState(false);
+    const [error, setError] = useState("")
+    const [submittedData, setSubmittedData] = useState(false);
     const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
     const hasItems = cartCtx.items.length>0;
 
-    const sendData = (data) => {
-        fetch("https://food-data-aff7b-default-rtdb.firebaseio.com/orders.json",
-        {
-            method: "post",
-            headers: {
-                'Content-Type':'application/json'
-            },
-            body: JSON.stringify({details:data, items:cartCtx.items})
+    const sendData = async (data) => {
+        
+        try{
+            setLoading(true);
+            const response = await fetch("https://food-data-aff7b-default-rtdb.firebaseio.com/orders.json",{
+                method: "post",
+                headers: {
+                    'Content-Type':'application/json'
+                },
+                body: JSON.stringify({details:data, items:cartCtx.items})
+            })
 
-        })
+            if(!response.ok){
+                throw new Error("")
+            }
+            setLoading(false);
+            setSubmittedData(true);
+        }catch(err){
+            setLoading(false);
+            setError("unable to process the order");
+        }
+        
     }
     
     const cartItemRemoveHandler = (id) => {
@@ -35,11 +51,15 @@ const Cart = (props) => {
 
     const handleOrder = () => setCheckout(true);
 
+    
+
     const orderCancel = <div className={classes.actions}>
                             {!hasItems && <p>There are no items in the cart</p>}
                             <button className={classes["button--alt"]} onClick = {props.onCancel}>Close</button>
                             {hasItems && <button onClick = {handleOrder} className={classes.button}>Order</button>}
                         </div>
+
+    
     
 
     const cartItems = <ul className={classes["cart-items"]}>{cartCtx.items.map((item) => 
@@ -51,8 +71,8 @@ const Cart = (props) => {
             onRemove = {cartItemRemoveHandler.bind(null, item.id)}
             onAdd = {cartItemAddHandler.bind(null, item)}
         />))}</ul>;
-    return (
-        <Modal onClose = {props.onCancel}>
+
+    const modalDisplay = <React.Fragment>
             {cartItems}
             <div className={classes.total}>
                 <span>Total Amount</span>
@@ -60,6 +80,13 @@ const Cart = (props) => {
             </div>
             {checkout && <Checkout onsubmit = {sendData} onCancel = {props.onCancel}/>}
             {!checkout && orderCancel}
+        </React.Fragment>
+   
+   
+   
+    return (
+        <Modal onClose = {props.onCancel}>
+            {isloading ? <p>loading...</p>:submittedData ? <p>The order was submitted</p>:error ? <p>{error}</p>:modalDisplay}
         </Modal>
     );
 };
--- src/components/Cart/checkout.js
+++ src/components/Cart/checkout.js
@@ -1,12 +1,50 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import classes from "./checkout.module.css"
 
 const Checkout = (props) => {
+
+    const [showerror, seterror] = useState(false)
+    const [isvalid, setValid] = useState({
+        name:false, 
+        street:false, 
+        city:false,
+        postal:false
+    });
+
+    const [isFocussed, setFocus] = useState({
+        name:false,
+        street:false,
+        city:false,
+        postal:false
+    })
 
     const nameinputref = useRef();
     const streetinputref = useRef();
     const cityinputref = useRef();
     const postalinputref= useRef();
+
+    
+
+
+    const changeHandler = (e) => {
+        const fieldname = e.target.name;
+        const fieldValue = e.target.value;
+        console.log(fieldValue)
+        if (fieldValue.trim() !== ''){
+            setValid({...isvalid, [fieldname]:true})
+        } else {
+            setValid({...isvalid,[fieldname]:false})
+        }
+        
+    }
+
+    const focusHandler = (e) => {
+        
+        
+        setFocus({...isFocussed, [e.target.name]:true})
+        
+    }
+
 
 
 
@@ -16,37 +54,42 @@ const Checkout = (props) => {
         const street = streetinputref.current.value;
         const city = cityinputref.current.value;
         const postal = postalinputref.current.value;
-        props.onsubmit({
-            name,
-            street,
-            city,
-            postal
-        })
+        if (isvalid.name && isvalid.street && isvalid.city && isvalid.postal){
+            props.onsubmit({
+                name,
+                street,
+                city,
+                postal
+            })
+        } else {
+           seterror(true);
+           
+        }
     };
 
     return (
         <form className = {classes.form} onSubmit={submitHandler}>
-            <div className={classes.control}>
+            {showerror?<p className={classes.error}>Please enter valid details</p>:null}
+            <div className={`${classes.control} ${!isvalid.name && isFocussed.name && classes.invalid}`}>
                 <label htmlFor="name">Your Name</label>
-                <input ref = {nameinputref} type="text" id="name"/>
+                <input ref = {nameinputref} type="text" id="name" name = "name" onChange = {changeHandler} onBlur = {focusHandler}/>
             </div>
-            <div className={classes.control}>
+            <div className={`${classes.control} ${!isvalid.street && isFocussed.street ? classes.invalid:null}`}>
                 <label htmlFor="Street">Street</label>
-                <input ref = {streetinputref} type="text" id="street"/>
+                <input ref = {streetinputref} type="text" id="street" name = "street" onChange = {changeHandler} onBlur = {focusHandler}/>
             </div>
-            <div className={classes.control}>
+            <div className={`${classes.control} ${!isvalid.city && isFocussed.city ? classes.invalid:null}`}>
                 <label htmlFor="city">City</label>
-                <input ref = {cityinputref} type="text" id="city"/>
+                <input ref = {cityinputref} type="text" id="city" name = "city" onChange = {changeHandler} onBlur = {focusHandler}/>
             </div>
-            <div className={classes.control}>
+            <div className={`${classes.control} ${!isvalid.postal && isFocussed.postal ? classes.invalid:null}`}>
                 <label htmlFor="code">PinCode</label>
-                <input ref = {postalinputref} type="text" id="code"/>
+                <input ref = {postalinputref} type="text" id="code" name = "postal" onChange = {changeHandler} onBlur = {focusHandler}/>
             </div>
             <div className={classes.actions}>
                 <button type="submit" onClick={props.onCancel}>Cancel</button>
                 <button className={classes.submit}>Submit</button>
             </div>
-            
         </form>
     )
 }
